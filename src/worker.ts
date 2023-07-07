8dee45821c4174304c4cce3b0fea8d259df4d9a1
Theros <theros@svaltek.xyz>
2023-07-07 21:51:47 +0100

    prisma

--- src/worker.ts
+++ src/worker.ts
@@ -1,4 +1,31 @@
 import apiRouter from './Router';
+import { PrismaClient } from '@prisma/client/edge'
+const prisma = new PrismaClient()
+
+addEventListener('fetch', (event) => {
+  event.respondWith(handleEvent(event))
+})
+
+async function handleEvent(event: FetchEvent): Promise<Response> {
+  const { request } = event
+
+  // waitUntil method is used for sending logs, after response is sent
+  event.waitUntil(
+    prisma.log
+      .create({
+        data: {
+          level: 'Info',
+          message: `${request.method} ${request.url}`,
+          meta: {
+            headers: JSON.stringify(request.headers),
+          },
+        },
+      })
+      .then()
+  )
+
+  return new Response(`request method: ${request.method}!`)
+}
 
 /**
  * - Run `npm run dev` in your terminal to start a development server
